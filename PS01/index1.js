4b2bae2fd21105ebafff4d364780fe35d36a49ab
Qingyue99 <31904741+Qingyue99@users.noreply.github.com>
2017-10-23 22:13:47 -0400

    week 6 bubble

--- PS01/index1.js
+++ PS01/index1.js
@@ -1,58 +1,136 @@
-var margin = {top: 30, right: 15, bottom: 80, left: 60}
+var margin = {top: 30, right: 15, bottom: 80, left: 60},
     outerWidth = 1100,
-        outerHeight = 700,
-    width = 1100 - margin.left - margin.right
+    outerHeight = 700,
+    width = 1100 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
-    .domain([0,6])
-    .range([ 0, width ]);
+    .range([ 0, width ]).nice();
 
 var y = d3.scale.linear()
-    .domain([0, 6])
-    .range([ height, 0 ]);
+    .range([ height, 0 ]).nice();
 
-var xValue = "frequency",
-        yValue = "nearby",
-        rValue = "gather(g)",
-        colorValue = "Manufacturer";
+var xCat = "frequency",
+        yCat = "nearby",
+        rCat = "percentage",
+        colorCat = "raw";
 
+d3.csv("bcc.csv", function(data){
+    data.forEach(function(d){
+        d.frequency = +d.frequency;
+        d.percentage = +d.percentage;
+        d.nearby = +d.nearby;
+    });
+    console.log(data);
+    var xMax = d3.max(data, function(d) { return d.frequency; }) * 2.7,
+        xMin = d3.min(data, function(d) { return d.frequency; }),
+        xMin = xMin > 0 ? 0 : xMin,
+        yMax = d3.max(data, function(d) { return d.nearby; }) * 2.7,
+        yMin = d3.min(data, function(d) { return d.nearby; }),
+        yMin = yMin > 0 ? 0 : yMin;
+    x.domain([xMin, xMax]);
+    y.domain([yMin, yMax]);
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .tickSize(-height);
 
-d3.csv("bubble chart compare", function(data) {
-    data.forEach(function(d) {
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickSize(-width);
 
-var svg = d3.select("body")
+    var color = d3.scale.category10();
+
+    var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function(d) {
+            return xCat + ": " + d[xCat] + "<br>" + yCat + ": " + d[yCat] + "<br>" + rCat + ": " + d[rCat];
+        });
+
+    var zoomBeh = d3.behavior.zoom()
+        .x(x)
+        .y(y)
+        .scaleExtent([0.5, 2])
+        .on("zoom", zoom);
+
+    var svg = d3.select("#bubble")
         .append("svg")
+        .attr("width", outerWidth)
+        .attr("height", outerHeight)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoomBeh);
+
+    svg.call(tip);
+
+    svg.append("rect")
         .attr("width", width)
         .attr("height", height);
 
-svg.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d)
-            {return (d.frequency)*0.7;})
-        .attr("cy", function(d)
-            {return (d.nearby)*0.7;})
-        .attr("r", function(d)
-{return Math.sqrt(h - d[1]);
-        });
-    svg.selectAll("text")
-        .data(data)
-        .enter()
+    svg.append("g")
+        .classed("x axis", true)
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
         .append("text")
-        .text(function(d) {
-            return d.frequency + "," + d.nearby;
-        })
-        .attr("x", function(d) {
-            return d.frequency;
-        })
-        .attr("y", function(d) {
-            return d.nearby;
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "12px")
-        .attr("fill", "red");
+        .classed("label", true)
+        .attr("x", width)
+        .attr("y", margin.bottom - 10)
+        .style("text-anchor", "end")
+        .text(xCat);
 
+    svg.append("g")
+        .classed("y axis", true)
+        .call(yAxis)
+        .append("text")
+        .classed("label", true)
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(yCat);
 
+    var objects = svg.append("svg")
+        .classed("objects", true)
+        .attr("width", width)
+        .attr("height", height);
+
+    objects.append("svg:line")
+        .classed("axisLine hAxisLine", true)
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", width)
+        .attr("y2", 0)
+        .attr("transform", "translate(0," + height + ")");
+
+    objects.append("svg:line")
+        .classed("axisLine vAxisLine", true)
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", 0)
+        .attr("y2", height);
+
+    objects.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .classed("dot", true)
+        .attr("r", function (d) { return 200*d[rCat]; })
+        .attr("transform", transform)
+        .style("fill", function(d) { return color(d[colorCat]); })
+        .on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
+
+    function zoom() {
+        svg.select(".x.axis").call(xAxis);
+        svg.select(".y.axis").call(yAxis);
+
+        svg.selectAll(".dot")
+            .attr("transform", transform);
+    }
+
+    function transform(d) {
+        return "translate(" + x(d[xCat]) + "," + y(d[yCat]) + ")";
+    }
 });
+
